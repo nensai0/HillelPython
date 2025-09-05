b03279dcecbdf69e05df350cb14ed882a6c4990d
Artem Horbeshko <artemhorbeshko777@gmail.com>
2025-09-05 20:55:51 +0200

    finis lesson04

--- lesson04/lesson04.py
+++ lesson04/lesson04.py
@@ -1,1 +1,22 @@
-print('Lesson 04')
+# a = []
+# b = [1, 2, 3]
+# c = [1,]
+# d = [1, 2, 3, None, 4, 5, 'a', True, 'b', 'c']
+# e = [1, 2, 3, [None, 4, 5, 'a'], [True, 'b', 'c'], None, 'Hello, world!']
+#
+# print(len(a))
+# print(b)
+# print(c)
+# print(d)
+
+# e = [1, 2, 3, [None, 4, 5, 'a'], [True, 'b', 'c'], None, 'Hello, world!']
+
+# print(len(e))
+# print(e[4][1])
+#
+# print(e[len(e)-1])
+# print(e[-1])
+
+c = [1, '66', 'ER', 66, 79, 12, 11, 1, 0, 5]
+
+print(c[1:9])
---
+++ lesson04/lesson4(1).py
@@ -0,0 +1,28 @@
+# my_list = [1, 6.77, 55, -66, 79, 66]
+# my_list_2 = [1, 6.77, 0, None, False, [], 0.01]
+#
+# print(sum(my_list))
+# print(min(my_list))
+# print(max(my_list))
+#
+# print(any(my_list_2))
+# print(all(my_list_2))
+#
+# print(sorted(my_list, reverse=True))
+# print(sorted(my_list, reverse=False))
+
+# number = 10
+# result = 1
+#
+# while number > 1:
+#     result *= number
+#     number -= 1
+# print(result)
+
+number = 10
+result = 1
+
+while number > 1:
+    result *= number
+    number -= 1
+print(result)
