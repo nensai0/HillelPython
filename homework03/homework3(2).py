a2e019863e654ae388464b73971da1852ee47e4e
Artem Horbeshko <artemhorbeshko777@gmail.com>
2025-09-08 17:24:47 +0200

    edit homework3(2)

--- homework03/homework3(2).py
+++ homework03/homework3(2).py
@@ -2,11 +2,7 @@ list1 = [12, 34, 56, 78, 'abc']
 
 if len(list1) <= 1:
     result = list1
-elif len(list1) == 1:
-    result = list1
 else:
-    last_element = list1[-1]
-    element1 = list1[:-1]
-    result = [last_element] + element1
+    result = [list1[-1]] + list1[:-1]
 
 print(result)
