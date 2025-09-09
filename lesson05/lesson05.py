fcb8e0af781c2df0c38b0d592fc6ad191a650991
Artem Horbeshko <artemhorbeshko777@gmail.com>
2025-09-09 19:48:18 +0200

    finish lesson 5

--- lesson05/lesson05.py
+++ lesson05/lesson05.py
@@ -1,1 +1,59 @@
-print('Lesson 05')
+# a = None
+#
+# while True:
+#     number = int(input("enter a number (10 - 20)"))
+#     if 10 <= number <= 20:
+#         break
+#
+# print("Try again!")
+from random import random
+
+# my_list = [1, 4, 7.5, -9, 0, 3, 5, 6, 8, 2, 11, 15, -4, 0,5]
+#
+# list_int = []
+# list_float = []
+# list_negative = []
+#
+# i = 0
+#
+# while i < len(my_list):
+#     if isinstance(my_list[i], (int, float)) and my_list[i] < 0:
+#         list_negative.append(my_list[i])
+#     elif isinstance(my_list[i], int):
+#         list_int.append(my_list[i])
+#     elif isinstance(my_list[i], float):
+#         list_float.append(my_list[i])
+#     i += 1
+#
+# print(list_int)
+# print(list_float)
+# print(list_negative)
+
+# my_list = [1, 4, 7.5, -9, 0, 3, 5, 6, 8, 2, 11, 15, -4, 0,5]
+#
+# list_int = []
+# list_float = []
+# list_negative = []
+#
+# for item in my_list:
+#     if isinstance(item, (int, float)) and item < 0:
+#         list_negative.append(item)
+#     elif isinstance(item, int):
+#         list_int.append(item)
+#     elif isinstance(item, float):
+#         list_float.append(item)
+# else:
+#     print("for else")
+#
+# print(list_int)
+# print(list_float)
+# print(list_negative)
+
+# a = 6
+# out_list = []
+#
+# for i in range(2, a + 1, 1):
+#     out_list.append(i)
+#
+# print(out_list)
+
